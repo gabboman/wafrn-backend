82f04fa06107f5d211b22377855c22efe9caf0f7
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2022-05-29 00:01:53 +0200

    good debug

--- index.ts
+++ index.ts
@@ -66,7 +66,7 @@ app.use(cors());
 const sequelize = new Sequelize(
     environment.databaseConnectionString,
     {
-      logging: !environment.prod,
+      logging: environment.prod ? false: console.log,
     },
 );
 
