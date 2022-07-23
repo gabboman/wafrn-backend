b30f820cfbb678594af4151214151ffda0813aad
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2022-07-23 22:14:38 +0100

    fix for sendinblue

--- exportContacts.ts
+++ exportContacts.ts
@@ -1,18 +1,18 @@
 import {
-    User
+  User,
 } from './models';
 
 
 User.findAll({
-    where: {
-        activated: 1
-    },
+  where: {
+    activated: 1,
+  },
 }).then((users: any) => {
-    console.log("EMAIL,FIRSTNAME,PHONE,CREATION_DATE,GENDER");
-    users.forEach((singleUser: any) => {
-        const csvLine = singleUser.email + ',' +
-            singleUser.url.replace(',', '') + ',' +
-            singleUser.createdAt.getTime() + ',1';
-        console.log(csvLine);
-    })
+  console.log('EMAIL,FIRSTNAME');
+  users.forEach((singleUser: any) => {
+    const csvLine = singleUser.email + ',' +
+            singleUser.url.replace(',', '') /* + ',' +
+            singleUser.createdAt + ',1'*/;
+    console.log(csvLine);
+  });
 });
