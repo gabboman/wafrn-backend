856f0d3e6b5be90023009b19458546a28fa977a9
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-05-19 18:56:55 +0200

    fixed again

--- index.ts
+++ index.ts
@@ -62,7 +62,7 @@ app.get('/api/dashboard', authenticateToken, async (req: any, res) => {
       createdAt: { [Op.lt]: getStartScrollParam(req) },
       privacy: { [Op.in]: [0, 1] },
       literal: sequelize.literal(
-        `"userId" in (select "followedId" from follows where "followerId" like "${posterId}") OR "userId" like '${posterId}'`
+        `"userId" in (select "followedId" from follows where "followerId" like '${posterId}') OR "userId" like '${posterId}'`
       )
     }
   })
