8d9ff19e2aad32e5ca209cc7a12ad7ee4c50725c
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-05-21 00:56:31 +0200

    job to update users

--- indexWorkers.ts
+++ indexWorkers.ts
@@ -2,15 +2,26 @@ import { Job, Worker } from 'bullmq'
 import { logger } from './utils/logger'
 import { environment } from './environment'
 import { inboxWorker } from './utils/queueProcessors/inbox'
+import { updateUserWorker } from './utils/queueProcessors/updateUser'
 
-const worker = new Worker('inbox', (job: Job) => inboxWorker(job), {
+const workerInbox = new Worker('inbox', (job: Job) => inboxWorker(job), {
   connection: environment.bullmqConnection,
   concurrency: environment.fediverseConcurrency
 })
-worker.on('completed', (job) => {
+
+const workerUpdateRemoteUsers = new Worker('updateUser', (job: Job) => updateUserWorker(job), {
+  connection: environment.bullmqConnection,
+  concurrency: environment.fediverseConcurrency
+})
+
+workerInbox.on('completed', (job) => {
   console.log(`${job.id} has completed!`)
 })
 
-worker.on('failed', (job, err) => {
+workerInbox.on('failed', (job, err) => {
+  console.warn(`${job?.id} has failed with ${err.message}`)
+})
+
+workerUpdateRemoteUsers.on('failed', (job, err) => {
   console.warn(`${job?.id} has failed with ${err.message}`)
 })
--- utils/queueProcessors/updateUser.ts
+++ utils/queueProcessors/updateUser.ts
@@ -1,32 +1,24 @@
-import { Worker } from 'bullmq'
+import { Job, Worker } from 'bullmq'
 import { getPetitionSigned } from '../activitypub/getPetitionSigned'
 import { User } from '../../db'
 import { environment } from '../../environment'
 import { logger } from '../logger'
 
-const updateUserWorker = new Worker(
-  'UpdateUsers',
-  async (job) => {
-    try {
-      const userPetition = await getPetitionSigned(job.data.petitionBy, job.data.userToUpdate)
-      const remoteUser = User.cache(job.data.userToUpdate).findOne({
-        where: {
-          remoteId: job.data.userToUpdate
-        }
-      })
-      remoteUser.description = userPetition.summary
-      remoteUser.avatar = userPetition.icon?.url
-        ? userPetition.icon.url
-        : `${environment.mediaUrl}/uploads/default.webp`
-      remoteUser.updatedAt = new Date()
-      await remoteUser.save()
-    } catch (error) {
-      logger.info(`Failed to update user ${job.data.userToUpdate}`)
-    }
-  },
-  {
-    connection: environment.bullmqConnection
+async function updateUserWorker(job: Job) {
+  try {
+    const userPetition = await getPetitionSigned(job.data.petitionBy, job.data.userToUpdate)
+    const remoteUser = User.cache(job.data.userToUpdate).findOne({
+      where: {
+        remoteId: job.data.userToUpdate
+      }
+    })
+    remoteUser.description = userPetition.summary
+    remoteUser.avatar = userPetition.icon?.url ? userPetition.icon.url : `${environment.mediaUrl}/uploads/default.webp`
+    remoteUser.updatedAt = new Date()
+    await remoteUser.save()
+  } catch (error) {
+    logger.info(`Failed to update user ${job.data.userToUpdate}`)
   }
-)
+}
 
 export { updateUserWorker }
