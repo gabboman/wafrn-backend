ee6193875315020676f0da20c16064e7b64a147e
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-05-28 17:44:31 +0200

    Big refactor to send posts in a better way

--- utils/activitypub/sendRemotePost.ts
+++ utils/activitypub/sendRemotePost.ts
@@ -1,38 +1,84 @@
-import { Op } from 'sequelize'
+import { Op, Sequelize } from 'sequelize'
 import { logger } from '../logger'
 import { postPetitionSigned } from './postPetitionSigned'
 import { postToJSONLD } from './postToJSONLD'
 import { LdSignature } from './rsa2017'
-import { User, sequelize } from '../../db'
+import { FederatedHost, User, sequelize } from '../../db'
 import getRemoteFollowers from './getRemoteFollowers'
 import { environment } from '../../environment'
-const _ = require('underscore')
 
 async function sendRemotePost(localUser: any, post: any) {
-  let usersToSendThePost = await getRemoteFollowers(localUser)
-  if (post.privacy / 1 === 10) {
-    const userIdsToSendPost = (await post.getPostMentionsUserRelations()).map((mention: any) => mention.userId)
-    const mentionedUsersFullModel = await User.findAll({
-      where: {
-        id: { [Op.in]: userIdsToSendPost },
-        remoteInbox: { [Op.ne]: null }
-      }
-    })
-    usersToSendThePost = _.groupBy(mentionedUsersFullModel, 'federatedHostId')
+  // servers with shared inbox
+  let serversToSendThePost;
+  // for servers with no shared inbox
+  let usersToSendThePost;
+  // mentioned users
+  const mentionedUsers = await User.findAll({
+    attributes: ['remoteInbox'],
+    where: {
+      federatedHostId: {[Op.ne]: null},
+      literal: Sequelize.literal(`id IN (SELECT userId FROM postMentionsUserRelations WHERE postId = "${post.id}")`)
+    }
+  });
+  switch(post.privacy) {
+    case 1: {
+      serversToSendThePost = await FederatedHost.findAll({
+        where: {
+          publicInbox: {[Op.ne]: null},
+          blocked: {[Op.ne]: true},
+          literal: sequelize.literal(`id in (SELECT federatedHostId from users where users.id IN (SELECT followerId from follows where followedId = '${post.userId}') and federatedHostId is not NULL)`)
+        }
+      })
+      usersToSendThePost = usersToSendThePost = await FederatedHost.findAll({
+        where: {
+          publicInbox: {[Op.eq]: null},
+          blocked: false
+        },
+        include: [
+          {
+            model: User,
+            attributes: ['remoteInbox'],
+            where: {
+              banned: false,
+              literal: Sequelize.literal(`users.id IN (SELECT followerId from follows where followedId = "${post.userId}")`)
+            }
+          }
+        ]
+      });
+
+      break;
+    }
+    case 10: {
+      serversToSendThePost = []
+      usersToSendThePost = []
+      break;
+    }
+    default: {
+      serversToSendThePost = await FederatedHost.findAll({
+        where: {
+          publicInbox: {[Op.ne]: null},
+          blocked: false
+        }
+      });
+      usersToSendThePost = await FederatedHost.findAll({
+        where: {
+          publicInbox: {[Op.eq]: null},
+          blocked: false
+        },
+        include: [
+          {
+            model: User,
+            attributes: ['remoteInbox'],
+            where: {
+              banned: false
+            }
+          }
+        ]
+      });
+    }
   }
 
-  if (post.privacy / 1 === 0) {
-    const allUserInbox = await User.findAll({
-      where: {
-        remoteInbox: { [Op.and]: [{ [Op.ne]: null }, { [Op.ne]: 'DELETED_USER' }] },
-        activated: true,
-        banned: false,
-        literal: sequelize.literal('federatedHostId not in (SELECT id from federatedHosts where blocked = 1)')
-      }
-    })
-    usersToSendThePost = _.groupBy(allUserInbox, 'federatedHostId')
-  }
-  if (usersToSendThePost && Object.keys(usersToSendThePost).length > 0) {
+  if (serversToSendThePost?.length > 0 || usersToSendThePost?.length > 0 || mentionedUsers?.length > 0) {
     const objectToSend = await postToJSONLD(post)
     const ldSignature = new LdSignature()
     const bodySignature: any = await ldSignature.signRsaSignature2017(
@@ -43,27 +89,26 @@ async function sendRemotePost(localUser: any, post: any) {
       new Date(post.createdAt)
     )
 
-    for (const remoteHost of Object.keys(usersToSendThePost)) {
-      let remainingUsers = 5 // we send a post up to 5 times. may work may wont work
-      for await (const remoteuser of usersToSendThePost[remoteHost]) {
-        try {
-          const response = await postPetitionSigned(
-            { ...objectToSend, signature: bodySignature.signature },
-            localUser,
-            remoteuser.remoteInbox
-          )
-          //const response = await postPetitionSigned(objectToSend, localUser, remoteuser.remoteInbox)
-          remainingUsers--
-          if (remainingUsers === 0) {
-            break
-          }
-        } catch (error) {
-          logger.trace({
-            message: 'Could not send post to remote user',
-            url: remoteuser.remoteInbox,
-            error: error
-          })
-        }
+    let inboxes: string[] = [];
+    inboxes = inboxes.concat(mentionedUsers.map((elem: any) => elem.remoteInbox))
+    inboxes = inboxes.concat(serversToSendThePost.map((elem: any) => elem.publicInbox))
+    usersToSendThePost?.forEach((server: any) => {
+      inboxes =  inboxes.concat(server.users.map((elem: any) => elem.remoteInbox))
+    });
+    
+    for (const remoteInbox of inboxes) {
+      try {
+        
+        postPetitionSigned(
+          { ...objectToSend, signature: bodySignature.signature },
+          localUser,
+          remoteInbox
+          
+        ).catch(error => {
+          logger.debug(error)
+        })
+      } catch (bigError) {
+        logger.debug(bigError)
       }
     }
   }
