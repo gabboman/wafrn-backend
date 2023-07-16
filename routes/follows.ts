54a9db00bc4817652905efb32f7bc58a5d5baa82
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-07-16 17:07:11 +0200

    give user the list of users they have blocked

--- routes/follows.ts
+++ routes/follows.ts
@@ -3,7 +3,6 @@ import { Blocks, User } from '../db'
 import { authenticateToken } from '../utils/authenticateToken'
 
 import getBlockedIds from '../utils/getBlockedIds'
-import getFollowedsIds from '../utils/getFollowedsIds'
 import { logger } from '../utils/logger'
 import { remoteFollow } from '../utils/activitypub/remoteFollow'
 import { remoteUnfollow } from '../utils/activitypub/remoteUnfollow'
@@ -103,11 +102,10 @@ export default function followsRoutes(app: Application) {
         )
       }
     })
-    //const blockedUsers = getBlockedIds(req.jwtData?.userId)
+    const blockedUsers = getBlockedIds(req.jwtData?.userId as (string))
     res.send({
       followedUsers: followedUsers.map((elem: any) => elem.id).concat(req.jwtData?.userId),
-      //blockedUsers: await blockedUsers
-      blockedUsers: []
+      blockedUsers: await blockedUsers
     })
   })
 }
--- utils/getBlockedIds.ts
+++ utils/getBlockedIds.ts
@@ -1,19 +1,12 @@
-import { User } from '../db'
+import { Blocks, User } from '../db'
 
 export default async function getBlockedIds(userId: string): Promise<string[]> {
   try {
-    const usr = await User.findOne({
+    return await Blocks.findAll({
       where: {
-        id: userId
-      },
-      attributes: ['id']
+        blockerId: userId
+      }
     })
-    const blocked = usr.getBlocked()
-    const blockedBy = usr.getBlocker()
-    await Promise.all([blocked, blockedBy])
-    let result = (await blocked).map((blocked: any) => blocked.id)
-    result = result.concat((await blockedBy).map((blocker: any) => blocker.id))
-    return result.filter((elem: string) => elem !== userId)
   } catch (error) {
     return []
   }
