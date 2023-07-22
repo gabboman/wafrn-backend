fa59cae7e278f3fd896511eb5b063d348f5ac460
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2023-07-22 20:26:06 +0200

    explaining what things do

--- environment.example.ts
+++ environment.example.ts
@@ -1,46 +1,62 @@
 export const environment = {
-  prod: false,
+  prod: true,
+  // this makes the logs really heavy
   logSQLQueries: false,
+  // VERY IMPORTANT. This to true DESTROYS YOUR FUCKING DATABASE. DO NOT. DO NOT. DO NOT. You set it to true the first time, create the admin user, then stop, set forcesync to false and enjoy
   forceSync: false,
   enableFediverse: true,
   workers: {
+    // if you set this to true, workers will start in the main thread. no need for starting the utils/workers.ts in other tmux tab
+    mainThread: true,
     low: 2,
     medium: 10,
     high: 25
   },
-  databaseConnectionString: 'mariadb://user:password@127.0.0.1/wafrn',
+  //YOU SHOULD TOTALLY USE MYSQL AND DO NOT COMMIT THE SAME MISTAKES AS ME. Mariadb works, but there were some performance issues because indexes
+  databaseConnectionString: 'mysql://user:password@127.0.0.1/wafrn',
   port: 3000,
-  fediPort: 6000,
   listenIp: '127.0.0.1',
   saltRounds: 12,
+  // for jwt secret you should use something like https://www.grc.com/passwords.htm please this is SUPER DUPER SECRET.
   jwtSecret: 'JWTSECRET',
+  // https://app.wafrn.net
   frontendUrl: 'FEDIVERSE_URL_WITH_HTTPS',
+  // app.wafrn.net
   instanceUrl: 'FEDIVERSE_URL_NO_PROTOCOL',
+  // https://media.wafrn.net
   mediaUrl: 'MEDIA_URL_OR_API_URL',
-  fronendToProxyUrl: 'FRONTEND_URL',
+  // You should run also this project github.com/gabboman/fediversemediacacher. In my case, https://cache.wafrn.net/?media=
   externalCacheurl: 'FEDIVERSE_CACHINATOR',
-  removeFolderNameFromFileUploads: false,
+  // this was a dev thing. leave to true unless you are doing stuff in local or your media url is yourinstance/uploads (not recomended)
+  removeFolderNameFromFileUploads: true,
+  // after the first run, create the admin user. update the environment
   adminUser: 'ADMIN_USER_URL',
+  // in MB. Please make sure you have the same in the frontend
   uploadSize: 50,
+  // 20 is a good number. more could take too much to load, 10 might be "faster" but you do loads more often and you see the loader a bit more
   postsPerPage: 20,
+  // The frontend files. Used if you route some things like /post/ through this. Read the manual because there is a way of updating the front without downtime
   frontedLocation: '/Users/gabriel/Code/wafrn/wafrn/dist/wafrn/browser',
+  // trace is extreme logging. debug is ok for now
   logLevel: 'debug',
+  // we now are not sure if its a good enough blocklist. There is a script that loads the file from here.
   blocklistUrl: 'https://codeberg.org/oliphant/blocklists/raw/branch/main/blocklists/_unified_tier0_blocklist.csv',
+  // oh yes, you need redis
   bullmqConnection: {
     host: 'localhost',
     port: 6379
   },
+  // this will create a backendlog.log file on the folder superior to this one. 
   pinoTransportOptions: {
     targets: [
-      {
-        target: 'pino/file',
-        level: 'error',
-        options: {
-          destination: 'ROUTE-TO-FILE'
-        }
-      }
-    ]
+      { target: 'pino/file', level: 0, options: {
+        destination: 'logs/backendlog.log'
+      } },
+    ],
   },
+  // you can try with gmail but we actually use sendinblue for this. bear in mind that this might require some fiddling in your gmail account too
+  // you might need to enable https://myaccount.google.com/lesssecureapps
+  // https://miracleio.me/snippets/use-gmail-with-nodemailer/
   emailConfig: {
     host: 'smtp_host',
     port: 25,
