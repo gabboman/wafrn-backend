f83ef4ae598e80f4a6c564f65ff6b79e1fe461cf
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-01-09 22:26:46 +0100

    pdfs not pics. also lint

--- utils/optimizeMedia.ts
+++ utils/optimizeMedia.ts
@@ -10,11 +10,12 @@ export default function optimizeMedia(inputPath: string): string {
   fileAndExtension[1] = 'webp'
   let outputPath = fileAndExtension.join('.')
   switch (originalExtension) {
+    case 'pdf':
     case 'webp':
       break
     case 'gif':
       // eslint-disable-next-line no-unused-vars
-      const gifConversion = new FfmpegCommand(inputPath)
+      new FfmpegCommand(inputPath)
         .addOption('-loop', '0')
         .save(outputPath)
         .on('end', () => {
@@ -27,19 +28,22 @@ export default function optimizeMedia(inputPath: string): string {
       break
     case 'mp4':
       fileAndExtension[0] = `${fileAndExtension[0]}_processed`
+    // eslint-disable-next-line no-fallthrough
     case 'webm':
     case 'mov':
     case 'mkv':
       fileAndExtension[1] = 'mp4'
       outputPath = fileAndExtension.join('.')
       // eslint-disable-next-line no-unused-vars
-      const command = new FfmpegCommand(inputPath)
+      new FfmpegCommand(inputPath)
         .videoCodec('libx264')
         .audioCodec('aac')
         .save(outputPath)
         .on('end', () => {
           try {
-            fs.unlinkSync(inputPath, () => {})
+            fs.unlinkSync(inputPath, () => {
+              logger.trace('media converted')
+            })
           } catch (exc) {
             logger.warn(exc)
           }
@@ -53,7 +57,9 @@ export default function optimizeMedia(inputPath: string): string {
         .write(outputPath, (err: any) => {
           if (!err) {
             try {
-              fs.unlinkSync(inputPath, () => {})
+              fs.unlinkSync(inputPath, () => {
+                logger.trace('media converted')
+              })
             } catch (exc) {
               logger.warn(exc)
             }
