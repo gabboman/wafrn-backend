4d2e2f0f04b9076dc37fdbd124dbf89b2411d67d
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-02-29 19:59:24 +0100

    fix /fediverse/ in regular posts, allow to fetch posts with regular url in THEORY

--- utils/overrideContentType.ts
+++ utils/overrideContentType.ts
@@ -1,8 +1,22 @@
 import { Request, Response, NextFunction } from 'express'
 
 export default function overrideContentType(req: Request, res: Response, next: NextFunction) {
+  const UrlPath = req.path
   if (req.headers['content-type'] && req.headers['content-type'] === 'application/activity+json') {
-    req.headers['content-type'] = 'application/json;charset=UTF-8'
+    // its a mastodon server asking for stuff
+    if (UrlPath.startsWith('/fediverse/') || UrlPath.startsWith('.well-known')) {
+      req.headers['content-type'] = 'application/json;charset=UTF-8'
+      next()
+    } else {
+      res.redirect(`/fediverse/${UrlPath}`)
+      res.send()
+    }
+  } else {
+    // its an user asking for the location
+    if (UrlPath.startsWith('/fediverse/')) {
+      res.redirect(UrlPath.split('/fediverse')[1])
+    } else {
+      next()
+    }
   }
-  next()
 }
