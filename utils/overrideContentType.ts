083ac7e7ba6c9a971cfddce3d7b18860ecd418b7
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-02-29 20:10:41 +0100

    try number 2

--- utils/overrideContentType.ts
+++ utils/overrideContentType.ts
@@ -2,23 +2,15 @@ import { Request, Response, NextFunction } from 'express'
 
 export default function overrideContentType(req: Request, res: Response, next: NextFunction) {
   const UrlPath = req.path
-  if (req.headers['content-type'] && req.headers['content-type'] === 'application/activity+json') {
-    req.headers['content-type'] = 'application/json;charset=UTF-8'
-    next()
-    /*
-    // its a mastodon server asking for stuff
-    if (UrlPath.startsWith('/fediverse/') || UrlPath.startsWith('.well-known')) {
-    } else {
-      res.redirect(`/fediverse/${UrlPath}`)
-      res.send()
-    }
-    */
-  } else {
+  if (req.headers.accept?.includes('*')) {
     // its an user asking for the location
     if (UrlPath.startsWith('/fediverse/')) {
       res.redirect(UrlPath.split('/fediverse')[1])
     } else {
       next()
     }
+  } else {
+    req.headers['content-type'] = 'application/json;charset=UTF-8'
+    next()
   }
 }
