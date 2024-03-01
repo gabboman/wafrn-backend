336035cf8601c108c4489afc59499ee4b3940423
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-03-01 15:47:05 +0100

    fix content type

--- utils/overrideContentType.ts
+++ utils/overrideContentType.ts
@@ -2,7 +2,9 @@ import { Request, Response, NextFunction } from 'express'
 
 export default function overrideContentType(req: Request, res: Response, next: NextFunction) {
   const UrlPath = req.path
-  req.headers['content-type'] = 'application/json;charset=UTF-8'
+  if (UrlPath.startsWith('/fediverse')) {
+    req.headers['content-type'] = 'application/json;charset=UTF-8'
+  }
   if (req.headers.accept?.includes('*/*')) {
     // its an user asking for the location
     if (UrlPath.startsWith('/fediverse/')) {
