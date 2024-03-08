39f4ec90491489f4295a29db60e2581013e8dde9
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-03-08 21:50:38 +0100

    removed checkfedisignature because it breaks in some cases

--- routes/activitypub/activitypub.ts
+++ routes/activitypub/activitypub.ts
@@ -38,7 +38,7 @@ function activityPubRoutes(app: Application) {
   // get post
   app.get(
     ['/fediverse/post/:id', '/fediverse/activity/post/:id'],
-    checkFediverseSignature,
+    //checkFediverseSignature,
     async (req: SignedRequest, res: Response) => {
       if (req.params?.id) {
         const post = await Post.findOne({
@@ -69,68 +69,72 @@ function activityPubRoutes(app: Application) {
     }
   )
   // Get blog for fediverse
-  app.get('/fediverse/blog/:url', checkFediverseSignature, async (req: SignedRequest, res: Response) => {
-    if (!req.params.url?.startsWith('@')) {
-      const url = req.params.url.toLowerCase()
-      const user = await getLocalUserByUrl(url)
-      if (user) {
-        const userForFediverse = {
-          '@context': ['https://www.w3.org/ns/activitystreams', 'https://w3id.org/security/v1'],
-          id: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}`,
-          type: 'Person',
-          following: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/following`,
-          followers: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers`,
-          featured: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/featured`,
-          inbox: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/inbox`,
-          outbox: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/outbox`,
-          preferredUsername: user.url.toLowerCase(),
-          name: user.name,
-          summary: user.description,
-          url: `${environment.frontendUrl}/blog/${user.url.toLowerCase()}`,
-          manuallyApprovesFollowers: false,
-          discoverable: true,
-          published: user.createdAt,
-          endpoints: {
-            sharedInbox: `${environment.frontendUrl}/fediverse/sharedInbox`
-          },
-          ...(user.avatar
-            ? {
-                icon: {
-                  type: 'Image',
-                  mediaType: 'image/webp',
-                  url: environment.mediaUrl + user.avatar
+  app.get(
+    '/fediverse/blog/:url',
+    //checkFediverseSignature,
+    async (req: SignedRequest, res: Response) => {
+      if (!req.params.url?.startsWith('@')) {
+        const url = req.params.url.toLowerCase()
+        const user = await getLocalUserByUrl(url)
+        if (user) {
+          const userForFediverse = {
+            '@context': ['https://www.w3.org/ns/activitystreams', 'https://w3id.org/security/v1'],
+            id: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}`,
+            type: 'Person',
+            following: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/following`,
+            followers: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers`,
+            featured: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/featured`,
+            inbox: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/inbox`,
+            outbox: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/outbox`,
+            preferredUsername: user.url.toLowerCase(),
+            name: user.name,
+            summary: user.description,
+            url: `${environment.frontendUrl}/blog/${user.url.toLowerCase()}`,
+            manuallyApprovesFollowers: false,
+            discoverable: true,
+            published: user.createdAt,
+            endpoints: {
+              sharedInbox: `${environment.frontendUrl}/fediverse/sharedInbox`
+            },
+            ...(user.avatar
+              ? {
+                  icon: {
+                    type: 'Image',
+                    mediaType: 'image/webp',
+                    url: environment.mediaUrl + user.avatar
+                  }
                 }
-              }
-            : undefined),
-          ...(user.headerImage
-            ? {
-                image: {
-                  type: 'Image',
-                  mediaType: 'image/webp',
-                  url: environment.mediaUrl + user.headerImage
+              : undefined),
+            ...(user.headerImage
+              ? {
+                  image: {
+                    type: 'Image',
+                    mediaType: 'image/webp',
+                    url: environment.mediaUrl + user.headerImage
+                  }
                 }
-              }
-            : undefined),
-          publicKey: {
-            id: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}#main-key`,
-            owner: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}`,
-            publicKeyPem: user.publicKey
+              : undefined),
+            publicKey: {
+              id: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}#main-key`,
+              owner: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}`,
+              publicKeyPem: user.publicKey
+            }
           }
-        }
 
-        res
-          .set({
-            'content-type': 'application/activity+json'
-          })
-          .send(userForFediverse)
+          res
+            .set({
+              'content-type': 'application/activity+json'
+            })
+            .send(userForFediverse)
+        } else {
+          return404(res)
+        }
       } else {
         return404(res)
       }
-    } else {
-      return404(res)
+      res.end()
     }
-    res.end()
-  })
+  )
 
   app.get('/fediverse/blog/:url/following', checkFediverseSignature, async (req: SignedRequest, res: Response) => {
     if (req.params?.url) {
@@ -195,92 +199,100 @@ function activityPubRoutes(app: Application) {
     res.end()
   })
 
-  app.get('/fediverse/blog/:url/followers', checkFediverseSignature, async (req: SignedRequest, res: Response) => {
-    if (req.params?.url) {
-      const url = req.params.url.toLowerCase()
-      const user = await getLocalUserByUrl(url)
-      if (user) {
-        const followersNumber = await User.count({
-          where: {
-            literal: sequelize.literal(`id in (SELECT followerId from follows where followedId like "${user.id}")`)
-          }
-        })
-        let response: any = {
-          '@context': 'https://www.w3.org/ns/activitystreams',
-          id: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers`,
-          type: 'OrderedCollectionPage',
-          totalItems: followersNumber,
-          first: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers?page=1`
-        }
-        if (req.query?.page && parseInt(req.query.page as string) > 0) {
-          const pageNumber = parseInt(req.query.page as string)
-          const maxPage = Math.floor(followersNumber / 10)
-          const followers = await User.findAll({
+  app.get(
+    '/fediverse/blog/:url/followers',
+    // checkFediverseSignature,
+    async (req: SignedRequest, res: Response) => {
+      if (req.params?.url) {
+        const url = req.params.url.toLowerCase()
+        const user = await getLocalUserByUrl(url)
+        if (user) {
+          const followersNumber = await User.count({
             where: {
               literal: sequelize.literal(`id in (SELECT followerId from follows where followedId like "${user.id}")`)
-            },
-            order: [['createdAt', 'DESC']],
-            limit: 10,
-            offset: (pageNumber - 1) * 10
+            }
           })
-          response = {
+          let response: any = {
             '@context': 'https://www.w3.org/ns/activitystreams',
             id: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers`,
-            type: 'OrderedCollection',
+            type: 'OrderedCollectionPage',
             totalItems: followersNumber,
-            partOf: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers`,
-            orderedItems: followers.map((elem: any) =>
-              elem.remoteId ? elem.remoteId : `${environment.frontendUrl}/fediverse/blog/${elem.url}`
-            )
+            first: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers?page=1`
           }
+          if (req.query?.page && parseInt(req.query.page as string) > 0) {
+            const pageNumber = parseInt(req.query.page as string)
+            const maxPage = Math.floor(followersNumber / 10)
+            const followers = await User.findAll({
+              where: {
+                literal: sequelize.literal(`id in (SELECT followerId from follows where followedId like "${user.id}")`)
+              },
+              order: [['createdAt', 'DESC']],
+              limit: 10,
+              offset: (pageNumber - 1) * 10
+            })
+            response = {
+              '@context': 'https://www.w3.org/ns/activitystreams',
+              id: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers`,
+              type: 'OrderedCollection',
+              totalItems: followersNumber,
+              partOf: `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers`,
+              orderedItems: followers.map((elem: any) =>
+                elem.remoteId ? elem.remoteId : `${environment.frontendUrl}/fediverse/blog/${elem.url}`
+              )
+            }
 
-          if (pageNumber > 1) {
-            response['prev'] = `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers?page=${
-              pageNumber - 1
-            }`
+            if (pageNumber > 1) {
+              response['prev'] = `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers?page=${
+                pageNumber - 1
+              }`
+            }
+            if (pageNumber < maxPage) {
+              response['next'] = `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers?page=${
+                pageNumber + 1
+              }`
+            }
           }
-          if (pageNumber < maxPage) {
-            response['next'] = `${environment.frontendUrl}/fediverse/blog/${user.url.toLowerCase()}/followers?page=${
-              pageNumber + 1
-            }`
-          }
+          res.set({
+            'content-type': 'application/activity+json'
+          })
+          res.send(response)
+        } else {
+          return404(res)
         }
-        res.set({
-          'content-type': 'application/activity+json'
-        })
-        res.send(response)
       } else {
         return404(res)
       }
-    } else {
-      return404(res)
+      res.end()
     }
-    res.end()
-  })
+  )
 
-  app.get('/fediverse/blog/:url/featured', checkFediverseSignature, async (req: SignedRequest, res: Response) => {
-    if (req.params?.url) {
-      const url = req.params.url.toLowerCase()
-      const user = await getLocalUserByUrl(url)
-      if (user) {
-        res.set({
-          'content-type': 'application/activity+json'
-        })
-        res.send({
-          '@context': 'https://www.w3.org/ns/activitystreams',
-          id: `${environment.frontendUrl}/fediverse/blog/${req.params.url}/featured`,
-          type: 'OrderedCollection',
-          totalItems: 0,
-          orderedItems: []
-        })
+  app.get(
+    '/fediverse/blog/:url/featured',
+    //checkFediverseSignature,
+    async (req: SignedRequest, res: Response) => {
+      if (req.params?.url) {
+        const url = req.params.url.toLowerCase()
+        const user = await getLocalUserByUrl(url)
+        if (user) {
+          res.set({
+            'content-type': 'application/activity+json'
+          })
+          res.send({
+            '@context': 'https://www.w3.org/ns/activitystreams',
+            id: `${environment.frontendUrl}/fediverse/blog/${req.params.url}/featured`,
+            type: 'OrderedCollection',
+            totalItems: 0,
+            orderedItems: []
+          })
+        } else {
+          return404(res)
+        }
       } else {
         return404(res)
       }
-    } else {
-      return404(res)
+      res.end()
     }
-    res.end()
-  })
+  )
 
   // HERE is where the meat and potatoes are. This endpoint is what we use to recive stuff
   app.post(
