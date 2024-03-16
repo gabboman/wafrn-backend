a3f3800b5504cf73f4593d90937edd19d82d5e7f
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2024-03-16 20:38:48 +0100

    Fixed cw stuff

--- utils/mailCampaing/mailCampaing.ts
+++ utils/mailCampaing/mailCampaing.ts
@@ -17,11 +17,12 @@ async function sendMail() {
   })
 
   for await (const user of users) {
-    const subject = `Hello ${user.url}, we miss you!`
+    const subject = `Hello ${user.url}, BE WARE OF THE IDES OF MARCH!`
     const body = `
     <h1>What's up ${user.url}, hope all is ok!😀</h1>
-    <p>We have done some biiig updates to <a href="https://app.wafrn.net" target="_blank">wafrn</a> lately, you should check us again!</p>
-    <p>It's been a loooong time since we send emails this way instead than a regular provider, so it might get lost<p>
+    <p>We have done some small updates to <a href="https://app.wafrn.net" target="_blank">wafrn</a> lately, you should check us again!</p>
+    <p>The updates do not include protection against knifes in the senate tho<p>
+    <img src="https://media.wafrn.net/1710404859891_554f3e53391f297ce47be10cc13a177eb971542b.webp">
     <p>Also, feel free to reply to this email if you have any questions, messages, or you just want to say "lol i dont even remember joining there lool"</p>
     `
     console.log(`mailing ${user.url}`)
