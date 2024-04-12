0854f2b024d32b4d95b9fc6bb3c84a5c8038429f
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2024-04-13 00:13:29 +0200

    updated mail campaing

--- utils/mailCampaing/mailCampaing.ts
+++ utils/mailCampaing/mailCampaing.ts
@@ -17,12 +17,11 @@ async function sendMail() {
   })
 
   for await (const user of users) {
-    const subject = `Hello ${user.url}, BE WARE OF THE IDES OF MARCH!`
+    const subject = `Hello ${user.url}, we made a huge update with the notifications page!`
     const body = `
     <h1>What's up ${user.url}, hope all is ok!😀</h1>
-    <p>We have done some small updates to <a href="https://app.wafrn.net" target="_blank">wafrn</a> lately, you should check us again!</p>
-    <p>The updates do not include protection against knifes in the senate tho<p>
-    <img src="https://media.wafrn.net/1710404859891_554f3e53391f297ce47be10cc13a177eb971542b.webp">
+    <p>We have done a huge update to <a href="https://app.wafrn.net" target="_blank">wafrn's notification page</a>, you should check it!</p>
+    <p>The new notifications page allows you to see </p>
     <p>Also, feel free to reply to this email if you have any questions, messages, or you just want to say "lol i dont even remember joining there lool"</p>
     `
     console.log(`mailing ${user.url}`)
